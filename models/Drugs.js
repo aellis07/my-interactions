e0dba67710b5d90eb96cc41859b72cbc1e907fbe
Anthony Ellis <amellis245@gmail.com>
2021-11-23 14:01:07 -0500

    created drug_name and drug_id fields for drug model

--- models/Drugs.js
+++ models/Drugs.js
@@ -3,4 +3,11 @@ const sequelize = require("../config/connection");
 
 class Drug extends Model {}
 
-Drug.init({});
+Drug.init({
+    drug_name: {
+        type: DataTypes.STRING,
+    },
+    drug_id: {
+        type: DataTypes.INTEGER,
+    },
+});
