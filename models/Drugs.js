05a9b7819233ab4a1e56ccd8c19051f938403c68
Anthony Ellis <amellis245@gmail.com>
2021-11-23 14:02:25 -0500

    linked model to database

--- models/Drugs.js
+++ models/Drugs.js
@@ -3,11 +3,14 @@ const sequelize = require("../config/connection");
 
 class Drug extends Model {}
 
-Drug.init({
-    drug_name: {
-        type: DataTypes.STRING,
+Drug.init(
+    {
+        drug_name: {
+            type: DataTypes.STRING,
+        },
+        drug_id: {
+            type: DataTypes.INTEGER,
+        },
     },
-    drug_id: {
-        type: DataTypes.INTEGER,
-    },
-});
+    { sequelize, timestamps: false, underscored: false, modelName: "drug" }
+);
