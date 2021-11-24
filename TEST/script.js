33f785c5c691ef951324006b1dfb80af32cc925e
Christopher Garcia <chris@ChristophersMBP.lan>
2021-11-24 09:08:37 -0500

    created getDrugId function to fetch the api for the id of the drug

--- TEST/script.js
+++ TEST/script.js
@@ -1,1 +1,12 @@
-
+const getDrugId = async () => {
+  const drug = "advil";
+  const result = await fetch(
+    `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${drug}&search=2`,
+    {
+      method: "GET",
+    }
+  );
+  const json = await result.json();
+  const drugId = json.idGroup.rxnormId[0];
+  console.log(drugId);
+};
