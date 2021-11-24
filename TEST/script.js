d94429b52f9f7be41df6412301f27185139547de
Christopher Garcia <chris@ChristophersMBP.lan>
2021-11-24 09:13:25 -0500

    created new fetch request to get the id for the second drug

--- TEST/script.js
+++ TEST/script.js
@@ -1,14 +1,24 @@
 const getDrugId = async () => {
   const drug = "advil";
+  const drug2 = "etanercept";
   const result = await fetch(
     `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${drug}&search=2`,
     {
       method: "GET",
     }
   );
+  const result2 = await fetch(
+    `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${drug2}&search=2`,
+    {
+      method: "GET",
+    }
+  );
   const json = await result.json();
   const drugId = json.idGroup.rxnormId[0];
+  const json2 = await result2.json();
+  const drugId2 = json2.idGroup.rxnormId[0];
   console.log(drugId);
+  getDrug(drugId);
 };
 const getDrug = async (drugId) => {
   const result = await fetch(
