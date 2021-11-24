e5a813c54cf402615cb501122cb184c283e1a137
Anthony Ellis <amellis245@gmail.com>
2021-11-24 10:56:50 -0500

    updated drugdata.json, switched sequelize force -> true

--- models/Drugs.js
+++ models/Drugs.js
@@ -4,24 +4,24 @@ const sequelize = require("../config/connection");
 class Drug extends Model {}
 
 Drug.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        drug_name: {
-            type: DataTypes.STRING,
-            unique: true,
-            allowNull: false,
-        },
-        drug_id: {
-            type: DataTypes.INTEGER,
-            unique: true,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    { sequelize, timestamps: false, underscored: false, modelName: "drug" }
+    drug_name: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    drug_id: {
+      type: DataTypes.INTEGER,
+      unique: true,
+      allowNull: false,
+    },
+  },
+  { sequelize, timestamps: false, underscored: false, modelName: "drug" }
 );
 module.exports = Drug;
