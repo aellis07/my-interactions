8fa3651d86bd7ea6720714c370a815f62099e40e
Christopher Garcia <chris@ChristophersMBP.lan>
2021-11-24 09:10:50 -0500

    created getdrug function to get the relationship that this drug shares with other drugs

--- TEST/script.js
+++ TEST/script.js
@@ -10,3 +10,24 @@ const getDrugId = async () => {
   const drugId = json.idGroup.rxnormId[0];
   console.log(drugId);
 };
+const getDrug = async (drugId) => {
+  const result = await fetch(
+    `https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=${drugId}&sources=DrugBank`,
+    {
+      method: "GET",
+    }
+  );
+  const json = await result.json();
+  const drugRelation =
+    json.interactionTypeGroup[0].interactionType[0].interactionPair;
+  for (let i = 0; i < drugRelation.length; i++) {
+    const drugId = drugRelation[i].interactionConcept[1].minConceptItem.rxcui;
+    const drugDescription = drugRelation[i].description;
+    const drugInformation = {
+      rxcui: drugId,
+      description: drugDescription,
+    };
+    console.log(drugInformation);
+  }
+  console.log(drugRelation);
+};
