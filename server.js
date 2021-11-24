ad0faf08ae1869f047b976f65897d18ad6c3c569
Anthony Ellis <amellis245@gmail.com>
2021-11-24 14:12:17 -0500

    -

---
+++ server.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const routes = require("./routes");
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+const sess = {
+  secret: "Super Secret Password",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+const hbs = exphbs.create();
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server. Force is set to false to not overwrite, decide as group if we want to change overwrite
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
+});
