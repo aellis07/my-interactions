85ff517b48cb193a887d8a726b2858baae848e87
Ricardo Garcia <ricardougarcia@gmail.com>
2021-11-25 13:11:02 -0500

    added redirect for users with accounts

--- routes/homeRoutes/login-routes.js
+++ routes/homeRoutes/login-routes.js
@@ -1,8 +1,69 @@
 const router = require("express").Router();
+const User = require("../../models/User");
 
-//  GET /login
-router.get("/", (req, res) => {
-  res.render("login");
+// Get login page
+router.get("/", async (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login to Account
+router.post("/", async (req, res) => {
+  try {
+    const dbUser = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!dbUser) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    }
+
+    const validPassword = await dbUser.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password. Please try again!" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+      res.redirect("dashboard");
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err.message);
+  }
+});
+
+// CREATE new user
+router.post("/", async (req, res) => {
+  try {
+    const dbUserData = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
