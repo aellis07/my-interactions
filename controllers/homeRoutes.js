acc5392f105e3a8ecf9924202aa6971164dcb29e
David Silveira <daviewave@gmail.com>
2021-11-28 15:15:11 -0500

    fixed naming issue where dashboard page was incorrectly named 'profile' and where public folder was lower cased

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -28,7 +28,7 @@ router.get("/", async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get("/profile", withAuth, async (req, res) => {
+router.get("/dashboard", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
@@ -38,7 +38,7 @@ router.get("/profile", withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render("profile", {
+    res.render("dashboard", {
       ...user,
       logged_in: true,
     });
@@ -50,7 +50,7 @@ router.get("/profile", withAuth, async (req, res) => {
 router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect("/profile");
+    res.redirect("/dashboard");
     return;
   }
 
