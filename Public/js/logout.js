c0bcbcf2eb20db8ee9e007c9edca1e054b08ea22
David Silveira <daviewave@gmail.com>
2021-11-27 22:05:10 -0500

    updated the code for logout.js in /public/js/ -- I think this works because i got the dashboard screen to appear when creating a new user and then i can logout in that case

--- Public/js/logout.js
+++ Public/js/logout.js
@@ -1,5 +1,5 @@
 const logout = async () => {
-  const response = await fetch("/logout", {
+  const response = await fetch("/api/users/logout", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
   });
@@ -7,9 +7,8 @@ const logout = async () => {
   if (response.ok) {
     document.location.replace("/");
   } else {
-    alert("Failed to log out.");
+    alert(response.statusText);
   }
 };
 
-// There is currently no logout button identified
 document.querySelector("#logout").addEventListener("click", logout);
