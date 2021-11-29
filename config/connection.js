a2065a31425c1c65f3fe36ab3b269a8f3b4c9cfb
Ricardo Garcia <ricardougarcia@gmail.com>
2021-11-29 16:18:48 -0500

    removed double sequl

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-const Sequelize = require("sequelize");
+
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
